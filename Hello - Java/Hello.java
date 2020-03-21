28cd11a459a59a757feb834de03f61e0051dfb07
Paula Aparecida Biotti da Silva <62397003+pauulabiotti@users.noreply.github.com>
2020-03-20 22:17:57 -0300

    Adicionei arquivos via upload

---
+++ Hello - Java/Hello.java
@@ -0,0 +1,10 @@
+/**
+    Hello World 
+    @author Paula Aparecida Biotti da Silva
+ */
+
+ public class Hello{
+     public static void main(String[] args){
+         System.out.println("Hello World");
+     }
+ }
